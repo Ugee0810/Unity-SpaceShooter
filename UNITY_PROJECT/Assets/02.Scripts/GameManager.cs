f1eecaa77bf2a58fb3a936fe1dbb5b78002ebaad
Ugee <85896566+Ugee99@users.noreply.github.com>
2022-08-16 15:51:10 +0900

    220816 15:51
    레인 사이즈 변경

--- UNITY_PROJECT/Assets/02.Scripts/GameManager.cs
+++ UNITY_PROJECT/Assets/02.Scripts/GameManager.cs
@@ -34,6 +34,7 @@ public class GameManager : MonoBehaviour
 
 
 
+
     public void StartNewGame()
     {
         StageLevel();
---
+++ UNITY_PROJECT/Assets/02.Scripts/Poolable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 풀링 할 프리팹에 이 스크립트를 붙여 구분
+/// </summary>
+
+public class Poolable : MonoBehaviour
+{
+	public bool IsUsing;
+}
