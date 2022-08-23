5f4397302af3256328aff3f2829e17c69d839754
Ugee <85896566+Ugee99@users.noreply.github.com>
2022-08-23 18:21:38 +0900

    220823 18:21

--- UNITY_PROJECT/Assets/02.Scripts/GameManager.cs
+++ UNITY_PROJECT/Assets/02.Scripts/GameManager.cs
@@ -2,13 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // GameManager.cs
 public class GameManager : MonoBehaviour
 {
+    [Header("[Scripts]")]
+    public MusicElements _MusicElements;
+
     [Header("[UI]")]
     public GameObject lobby;
     public GameObject inGame;
+
+    public GameObject scrollOriginal;
+    public GameObject originalMusicElement; // Original Btn Prefab
+    public GameObject scrollOriginalContent;
+
+    public GameObject scrollCustom;
+    public GameObject customMusicElement;   // Custom Btn Prefab
+    public GameObject scrollCustomContent;
+
+    //[SerializeField] Image musicAlbum;
+    //[SerializeField] Text txtTitle;
+    //[SerializeField] Text txtPlayTime;
+    //[SerializeField] AudioSource selectMusic;
 
     [Header("[SFX]")]
     public GameObject vizualizationObjects;
@@ -19,6 +36,7 @@ public class GameManager : MonoBehaviour
     public GameObject panelStartLight;
     public GameObject panelDestroyColl;
 
+
     [Header("[Music Start]")]
     public Transform panelSpawnPoint; // 패널 생성 위치
     public float beat;                // BPM
@@ -27,29 +45,21 @@ public class GameManager : MonoBehaviour
     [Header("[플래그 변수]")]
     [SerializeField] bool isStart = false;       // 인게임 진행 상태
     [SerializeField] bool isStop = false;        // 인게임 도중 일시 정지 상태
+
+    [SerializeField] bool isOriginal;    // Original Btn
+    [SerializeField] bool isCustom = false;      // Custom Btn
+
     [SerializeField] bool isLevelEasy = false;   // Level Easy
     [SerializeField] bool isLevelNormal = false; // Level Normal
     [SerializeField] bool isLevelHard = false;   // Level Hard
 
+
     [Header("[패널 프리팹]")]
     public EnumPanel[] Panels;
 
-    public AudioSource _AudioSource_Music;
-
     private void Awake()
     {
-        StartCoroutine(FileIo());
-
-    }
-
-    IEnumerator FileIo()
-    {
-        WWW tmpWWW = new WWW(@"file://" + @"C:/ wav / 01.wav");
-
-        while (!tmpWWW.isDone)
-            yield return null;
-
-        //_AudioSource_Music.clip = tmpWWW.GetAudioClip(true, false);
+        isOriginal = true;
     }
 
     private void Update()
@@ -103,4 +113,99 @@ public class GameManager : MonoBehaviour
         // 일시 정지 UI 출력
     }
 
+    object[] originalMusics;
+    object[] customMusics;
+
+    public void Original() // Btn Original
+    {
+        if (!isOriginal && isCustom)
+        {
+            scrollOriginal.SetActive(true);
+            scrollCustom.SetActive(false);
+            StartCoroutine(OriginalSearch());
+        }
+    }
+
+    public void Custom() // Btn Custom
+    {
+        if (!isCustom && isOriginal)
+        {
+            scrollOriginal.SetActive(false);
+            scrollCustom.SetActive(true);
+            StartCoroutine(CustomSearch());
+        }
+    }
+
+    IEnumerator OriginalSearch()
+    {
+        if (true)
+        {
+            Destroy(_MusicElements.gameObject);
+        }
+        isCustom = false;
+        isOriginal = true;
+
+        originalMusics = Resources.LoadAll<AudioClip>("Original Music"); // Custom Music 폴더의 AudioClip 속성 파일 조회
+
+        for (int i = 0; i < originalMusics.Length; i++)
+        {
+            GameObject originalMusicElements = originalMusics[i] as GameObject; // AudioClip to GameObject
+
+            originalMusicElements = Instantiate(originalMusicElement);
+            originalMusicElements.name = "Original Music Element_" + i;
+            originalMusicElements.transform.parent     = scrollOriginalContent.transform;
+            originalMusicElements.transform.localScale = Vector3.one;
+            originalMusicElements.transform.position   = scrollOriginalContent.transform.position;
+
+            // 프로젝트 폴더의 mp3 -> 오디오 소스의 클립에 저장
+            originalMusicElements.transform.GetChild(3).gameObject.GetComponent<AudioSource>().clip = (AudioClip)originalMusics[i];
+            // 오디오 소스의 텍스트를 타이틀 텍스트에 저장
+            originalMusicElements.transform.GetChild(1).gameObject.GetComponent<Text>().text = originalMusicElements.transform.GetChild(3).gameObject.GetComponent<AudioSource>().clip.name;
+        }
+
+        foreach (var t in originalMusics)
+        {
+            Debug.Log(t);
+        }
+        yield return new WaitForSeconds(1);
+        StopCoroutine(OriginalSearch());
+    }
+
+    IEnumerator CustomSearch()
+    {
+        Destroy(_MusicElements.gameObject);
+        isCustom = true;
+        isOriginal = false;
+
+        customMusics = Resources.LoadAll<AudioClip>("Custom Music"); // Custom Music 폴더의 AudioClip 속성 파일 조회
+
+        for (int i = 0; i < customMusics.Length; i++)
+        {
+            GameObject customMusicElements = customMusics[i] as GameObject; // AudioClip to GameObject
+
+            customMusicElements = Instantiate(customMusicElement);
+            customMusicElements.name = "Custom Music Element_" + i;
+            customMusicElements.transform.parent     = scrollCustomContent.transform;
+            customMusicElements.transform.localScale = Vector3.one;
+            customMusicElements.transform.position   = scrollCustomContent.transform.position;
+
+            // 프로젝트 폴더의 mp3 -> 오디오 소스의 클립에 저장
+            customMusicElements.transform.GetChild(3).gameObject.GetComponent<AudioSource>().clip = (AudioClip)customMusics[i];
+            // 오디오 소스의 텍스트를 타이틀 텍스트에 저장
+            customMusicElements.transform.GetChild(1).gameObject.GetComponent<Text>().text = customMusicElements.transform.GetChild(3).gameObject.GetComponent<AudioSource>().clip.name;
+
+            // 프리팹 하위 인스턴스 조회
+            //customMusicElements.transform.GetChild(0).gameObject.GetComponent<Image>();
+            //customMusicElements.transform.GetChild(1).gameObject.GetComponent<Text>();
+            //customMusicElements.transform.GetChild(2).gameObject.GetComponent<Text>();
+            //customMusicElements.transform.GetChild(3).gameObject.GetComponent<AudioSource>();
+        }
+
+        foreach (var t in customMusics)
+        {
+            Debug.Log(t);
+        }
+        yield return new WaitForSeconds(1);
+        StopCoroutine(CustomSearch());
+    }
 }
